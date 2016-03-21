b97c2218b8a9e755274f8eae4bbe07bf1a618eee
Alex Harries <alex@greyhead.co.uk>
2016-03-21 11:44:18 +0000

    Remove the multisite template submodule - we will add it during a build script instead.
    Signed-off-by: Alex Harries <alex@greyhead.co.uk>

--- deployment-scripts/updateremote.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-echo "This file has moved to https://github.com/alexharries/drupal-scripts-of-usefulness/blob/master/updateremote.sh"
-echo ""
-echo "Sorry about that... :("
-echo ""
